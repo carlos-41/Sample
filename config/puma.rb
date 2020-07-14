8c533f70dd23223fa223f9caac2447b8218b5689
Luis Carlos <luis.carlos.finde@gmail.com>
2020-07-14 02:36:05 -0500

    Missing puma files

--- config/puma.rb
+++ config/puma.rb
@@ -1,38 +1,20 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+rails_env = "production"
+environment rails_env
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+app_dir = "/home/ubuntu/Sample/shared"
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+bind  "unix://#{app_dir}/puma.sock"
+pidfile "#{app_dir}/puma.pid"
+state_path "#{app_dir}/puma.state"
+directory "#{app_dir}/"
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers 2
+threads 1,2
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+daemonize true
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+activate_control_app "unix://#{app_dir}/pumactl.sock"
+
+prune_bundler
